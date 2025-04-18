fafcdce4dd4e20eb8e01351d449de3bfb72e2ee4
Abhi16016 <abhinavkale919913@gmail.com>
2025-04-19 00:51:21 +0530

    Minor update

--- src/telegram.ts
+++ src/telegram.ts
@@ -16,43 +16,43 @@ interface TelegramResponse<T> {
 }
 
 export async function sendMessage(chatId: number, text: string) {
-  const resp = await axios.post<TelegramResponse<any>>(
+  const res = await axios.post<TelegramResponse<any>>(
     `${API_BASE}/sendMessage`,
     { chat_id: chatId, text }
   );
-  return resp.data.result;
+  return res.data.result;
 }
 
 export async function getUpdates(offset?: number, limit = 10) {
   const params: any = { limit };
   if (offset) params.offset = offset;
-  const resp = await axios.get<TelegramResponse<any>>(
+  const res = await axios.get<TelegramResponse<any>>(
     `${API_BASE}/getUpdates`,
     { params }
   );
-  return resp.data.result;
+  return res.data.result;
 }
 
 export async function sendPhoto(chatId: number, photoUrl: string, caption?: string) {
-  const resp = await axios.post<TelegramResponse<any>>(
+  const res = await axios.post<TelegramResponse<any>>(
     `${API_BASE}/sendPhoto`,
     { chat_id: chatId, photo: photoUrl, caption }
   );
-  return resp.data.result;
+  return res.data.result;
 }
 
 export async function getChat(chatId: number) {
-  const resp = await axios.get<TelegramResponse<any>>(
+  const res = await axios.get<TelegramResponse<any>>(
     `${API_BASE}/getChat`,
     { params: { chat_id: chatId } }
   );
-  return resp.data.result;
+  return res.data.result;
 }
 
 export async function deleteMessage(chatId: number, messageId: number) {
-  const resp = await axios.post<TelegramResponse<any>>(
+  const res = await axios.post<TelegramResponse<any>>(
     `${API_BASE}/deleteMessage`,
     { chat_id: chatId, message_id: messageId }
   );
-  return resp.data.ok;
+  return res.data.ok;
 } 
